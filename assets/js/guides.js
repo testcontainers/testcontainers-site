98600fbeed4cb32ab1584d2acacfdeb36a1b9860
Leo <leocrossuk@gmail.com>
2023-02-08 13:18:32 +0000

    Update assets/js/guides.js
    Co-authored-by: Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>

--- assets/js/guides.js
+++ assets/js/guides.js
@@ -1,36 +1,32 @@
 import Fuse from './fuse.js';
 
-const search = document.querySelector("#guides-search");
-const guidesList = document.querySelector(".guides-list-items");
-const allGuides = document.querySelectorAll(".guides-list-item");
-const searchEmpty = document.querySelector(".guides-search-empty");
+const search = document.querySelector('#guides-search');
+const guidesList = document.querySelector('.guides-list-items');
+const allGuides = document.querySelectorAll('.guides-list-item');
+const searchEmpty = document.querySelector('.guides-search-empty');
 
 const guides = [];
-console.log(guides);
 
-allGuides.forEach((guide) => {
-    guides.push({element: guide, data: JSON.parse(guide.dataset.search)});
-});
+allGuides.forEach((guide) => guides.push({ element: guide, data: JSON.parse(guide.dataset.search) }));
 
-const fuse = new Fuse(guides, {keys: ['data.title', 'data.description'], ignoreLocation: true});
+const fuse = new Fuse(guides, { keys: ['data.title', 'data.description'], ignoreLocation: true });
 
 function showSearchResults(searchTerm) {
-    if (searchTerm == "") {
+    if (!searchTerm) {
         searchEmpty.classList.remove('show');
-        return guidesList.replaceChildren(...guides.map(guide => guide.element));
+        return guidesList.replaceChildren(...guides.map((guide) => guide.element));
     }
+
     const result = fuse.search(searchTerm);
-    const resultElements = result.map(result => {
-        return result.item.element;
-    })
+    const resultElements = result.map((result) => result.item.element);
+
     guidesList.replaceChildren(...resultElements);
-    if(result.length == 0) {
+
+    if (!result.length) {
         searchEmpty.classList.add('show');
     } else {
         searchEmpty.classList.remove('show');
     }
 }
 
-search.addEventListener('keyup', e => {
-    showSearchResults(search.value);
-})
+search.addEventListener('keyup', (_) => showSearchResults(search.value));
