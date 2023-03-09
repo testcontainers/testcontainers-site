a86896f4b5435c40881728e6d5ba0ff1db0bb3a1
Leo <leocrossuk@gmail.com>
2023-03-09 14:23:30 +0000

    Add guides dev script

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ testcontainers.com website
 ## Development
 1. Install Go https://go.dev/
 2. Install Hugo (extended version) https://gohugo.io/
-3. From the project root run `hugo server`
+3. From the project root run `sh dev.sh` to populate the guides folder
+4. From the project root run `hugo server`
---
+++ dev.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "---------------- Downloading Guides -----------------------------"
+GIT_ORG="https://github.com/testcontainers"
+GUIDE_REPOS=(
+  'tc-guide-introducing-testcontainers'
+  'tc-guide-testing-spring-boot-rest-api'
+  'tc-guide-testcontainers-lifecycle'
+  'tc-guide-configuration-of-services-running-in-container'
+)
+
+GUIDE_REPOS_CLONE_DIR="./guide-repos"
+GUIDES_TARGET_DIR="./content/guides"
+
+rm -rf ${GUIDE_REPOS_CLONE_DIR}
+mkdir ${GUIDE_REPOS_CLONE_DIR}
+
+for repo_name in "${GUIDE_REPOS[@]}"; do
+  echo "Cloning ${GIT_ORG}/${repo_name}.git"
+  git clone "${GIT_ORG}/${repo_name}.git" "${GUIDE_REPOS_CLONE_DIR}/${repo_name}/"
+  rm -rf "${GUIDES_TARGET_DIR:?}/${repo_name}"
+  cp -r "${GUIDE_REPOS_CLONE_DIR}/${repo_name}/guide/." "${GUIDES_TARGET_DIR}"
+done
+echo "------------------Guides Setup Completed ---------------------------"
