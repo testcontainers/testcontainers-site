be92f8bd523a764abc0ea7816962562f6b81c92a
Leo <leocrossuk@gmail.com>
2023-04-11 15:28:49 +0100

    Update dev script

--- dev.sh
+++ dev.sh
@@ -3,10 +3,12 @@
 echo "---------------- Downloading Guides -----------------------------"
 GIT_ORG="https://github.com/testcontainers"
 GUIDE_REPOS=(
-  'tc-guide-introducing-testcontainers'
+   'tc-guide-introducing-testcontainers'
+  'tc-guide-getting-started-with-testcontainers-for-java'
   'tc-guide-testing-spring-boot-rest-api'
   'tc-guide-testcontainers-lifecycle'
   'tc-guide-configuration-of-services-running-in-container'
+  'tc-guide-replace-h2-with-real-database-for-testing'
 )
 
 GUIDE_REPOS_CLONE_DIR="./guide-repos"
