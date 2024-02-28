38c2e982a285b5eb46a88a19f8e6ce970a4bf4fc
Leo <leocrossuk@gmail.com>
2024-02-28 22:43:12 +0000

    Switch to sync

--- convert-svg.js
+++ convert-svg.js
@@ -1,15 +1,14 @@
 import { Magick } from "node-magickwand";
-import { glob } from "glob";
+import { globSync } from "glob";
 import * as path from "path";
 
-(async () => {
-    const files = await glob("content/modules/**/*.svg");
-    files.forEach(async (image) => {
-        let im = new Magick.Image;
-        await im.backgroundColorAsync(new Magick.Color(0,0,0,0));
-        await im.readAsync("230x230", image);
-        const dirName = path.basename(path.dirname(image));
-        await im.magickAsync("PNG");
-        await im.writeAsync(`assets/images/modules/share-logos/${dirName}.png`);
-    })
-})();
+const files = globSync("content/modules/**/*.svg");
+files.forEach((image) => {
+    let im = new Magick.Image;
+    im.backgroundColor(new Magick.Color(0,0,0,0));
+    im.read("230x230", image);
+    const dirName = path.basename(path.dirname(image));
+    im.magick("PNG");
+    im.write(`assets/images/modules/share-logos/${dirName}.png`);
+    console.log(`Converted: ${dirName}`)
+});
