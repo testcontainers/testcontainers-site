b7862881f0a7417563533027cf0f8cf1faf98773
Leo <leocrossuk@gmail.com>
2024-02-28 23:03:54 +0000

    debugging write error

--- convert-svg.js
+++ convert-svg.js
@@ -1,14 +1,23 @@
 import { Magick } from "node-magickwand";
 import { globSync } from "glob";
 import * as path from "path";
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+console.log(`__dirname: ${__dirname}`);
 
 const files = globSync("content/modules/**/*.svg");
+console.log("Files:");
+console.log(files);
 files.forEach((image) => {
+    const dirName = path.basename(path.dirname(image));
+    console.log(`Starting: ${dirName}`);
     let im = new Magick.Image;
     im.backgroundColor(new Magick.Color(0,0,0,0));
     im.read("230x230", image);
-    const dirName = path.basename(path.dirname(image));
     im.magick("PNG");
-    im.write(`assets/images/modules/share-logos/${dirName}.png`);
-    console.log(`Converted: ${dirName}`)
+    const writePath = `${__dirname}/assets/images/modules/share-logos/${dirName}.png`
+    console.log(`Writing: ${writePath}`)
+    im.write(writePath);
+    console.log(`Finished: ${dirName}`)
 });
