782e6cf1a73f3d1898e64c1046eba7d33bd27d8e
Leo <leocrossuk@gmail.com>
2024-02-29 11:39:09 +0000

    Convert community logos too once structure is updated

--- convert-svg.js
+++ convert-svg.js
@@ -2,7 +2,10 @@ import { globSync } from "glob";
 import * as path from "path";
 import sharp from "sharp";
 
-const files = globSync("content/modules/**/*.svg");
+const files = globSync([
+    "content/modules/**/*.svg",
+    "community-module-registry/modules/**/*.svg"
+]);
 files.forEach((image) => {
     const dirName = path.basename(path.dirname(image));
     const writePath = `assets/images/modules/share-logos/${dirName}.png`
